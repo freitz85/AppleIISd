27781e40f3130e7dc1b960ab9390658bdee1f0fa
Florian Reitz <freitz85@gmail.com>
2020-10-03 12:02:42 +0200

    make_image.sh improved

---
+++ Firmware/make_image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+make clean
+make OPTIONS=mapfile,listing
+java -jar ../Binary/AppleCommander-ac-1.5.0.jar -d ../Binary/Flasher.dsk appleiisd.bin
+java -jar ../Binary/AppleCommander-ac-1.5.0.jar -p ../Binary/Flasher.dsk appleiisd.bin $00 < AppleIISd.bin
+cp AppleIISd.bin ../Binary/
--- Software/make_image.sh
+++ Software/make_image.sh
@@ -2,5 +2,6 @@
 
 make clean
 make
-java -jar AppleCommander-ac-1.5.0.jar -d flasher.dsk flasher
-java -jar AppleCommander-ac-1.5.0.jar -as flasher.dsk flasher < flasher.bin
+java -jar ../Binary/AppleCommander-ac-1.5.0.jar -d ../Binary/Flasher.dsk flasher
+java -jar ../Binary/AppleCommander-ac-1.5.0.jar -as ../Binary/Flasher.dsk flasher < Flasher.bin
+cp Flasher.bin ../Binary/
